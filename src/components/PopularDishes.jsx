bdda727b541384d0a256fed7a4347b04fd07fdf3
gowtham <surugowtham27@gmail.com>
2024-12-17 23:36:24 +0530

    dish names

--- src/components/PopularDishes.jsx
+++ src/components/PopularDishes.jsx
@@ -10,31 +10,31 @@ const PopularDishes = () => {
         {
             id:1,
             pizza: pizza1,
-            name: "Peporoni Pizza",
+            name: "Hawaiian Pizza",
             desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo laudantium hic voluptas.",
             rating: 4
         },
         {
             id:2,
             pizza: pizza2,
-            name: "Sushi Pizza",
+            name: "pan Pizza",
             desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo laudantium hic voluptas.",
             rating: 4
         },
         {
             id:3,
             pizza: pizza3,
-            name: "Margarita Pizza",
+            name: "Margherita Pizza",
             desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo laudantium hic voluptas.",
-            rating: 4
+            rating: 4.5
         },
     ]
 
     return (
-        <div className='py-10 px-5 lg:px-0 lg:py-20 bg-gray-800' id='menu'>
-            <div className='text-center max-w-7xl mx-auto'>
-                <h1 className='text-3xl lg:text-4xl font-bold text-red-500'>Popular Dishes</h1>
-                <p className='lg:text-xl text-sm pt-2 text-white'>Check out our most popular and highly-rated dishes.</p>
+        <div className='px-5 py-10 bg-gray-800 lg:px-0 lg:py-20' id='menu'>
+            <div className='mx-auto text-center max-w-7xl'>
+                <h1 className='text-3xl font-bold text-red-500 lg:text-4xl'>Popular Dishes</h1>
+                <p className='pt-2 text-sm text-white lg:text-xl'>Check out our most popular and highly-rated dishes.</p>
                 <div className='grid grid-cols-1 lg:grid-cols-3 pt-11 gap-7'>
                     {
                         menu.map((item)=>{
